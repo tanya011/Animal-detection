7c40001fb38f9449c1c37b4109432293529b1969
de-shyt <de.shut@inbox.ru>
2023-10-12 23:57:14 +0200

    get video or live stream from youtube

--- src/frames.py
+++ src/frames.py
@@ -1,30 +1,40 @@
 import cv2
 import os
+from vidgear.gears import CamGear
 
 
 def get_frames(video_path):
-    cap = cv2.VideoCapture(video_path)
+    # Check if the source is a live stream or a video of `.mp4` format
+    stream_mode = True
+    if video_path[-4::] == '.mp4':
+        stream_mode = False
 
-    if not cap.isOpened():
-        raise "Error: Video file '{video_path}' not found or could not be opened."
+    # Open the source
+    src_video = CamGear(source=video_path, stream_mode=stream_mode)
+    src_video.start()
 
     # Directory where the frames are stored
     frames_dir = 'frames'
     os.makedirs(frames_dir, exist_ok=True)
 
+    # Remove old data from `frames_dir` directory
+    for file in os.listdir(frames_dir):
+        file_name = os.path.join(frames_dir, file)
+        os.remove(file_name)
+
+    # Get frames from the source
     frame_number = 0
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-
-        if not ret:
-            # End of video
-            break
-
-        frame_filename = os.path.join(frames_dir, f'frame_{frame_number:03d}.jpg')
-        cv2.imwrite(frame_filename, frame)
+    while True:
+        frame = src_video.read()
+        if frame is None:
+            break  # End of video
+        frame_name = os.path.join(frames_dir, f'frame_{frame_number:03d}.jpg')
+        cv2.imwrite(frame_name, frame)
         frame_number += 1
 
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+        # Used to avoid running out of memory when a live stream is the source.
+        if frame_number > 500:
+            break
+
+    # Close the source
+    src_video.stop()
--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 from frames import get_frames
+import os
 
-video_path = 'video.mp4'
+
+video_path = 'video.mp4'  # video stored locally
+# video_path = 'https://www.youtube.com/watch?v=SmFK04m9VTM'  # youtube video
+# video_path = 'https://youtu.be/EBer-aLmzM8'  # youtube live stream
 get_frames(video_path)
+
+files_amount = sum(1 for _ in os.listdir('frames'))
+print(f'{files_amount} files were created.')
